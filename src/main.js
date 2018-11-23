c30618699129ce3e15369d21ea663febaeba2c89
Dr. Pit Lazarus <drpitlazarus@gmail.com>
2018-11-23 18:43:58 -0500

    Minimal working leaflet version

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
 const L = require('leaflet')
+const terrBounds = require('./territory_bounds')
 
 
 let theMap = L.map('map', { 
     attributionControl: false,
-    center: [256, 256],
     crs: L.CRS.Simple,
-    maxBounds: [[-100,-100],[612,612]],
-    zoom: 2
+    center: [1024, 1024],
+    inertia: false,
+    minZoom: -1.5,
+    maxZoom: 1,
+    zoom: -.5,
+    zoomDelta: .5,
+    zoomSnap: .5
 })
 
 let attribution = L.control.attribution({ prefix: "Map Image &copy; Muse Games" }).addTo(theMap)
+attribution.addAttribution('<a href="https://leafletjs.com" target="_blank">Leaflet</a>')
 
-let imageUrl = "map.png",
-    imageBounds = [[0,0],[512,512]]
+let bounds = [[0, 0], [2048, 2048]]
+let mapImage = L.imageOverlay('map.png', bounds).addTo(theMap)
 
-let imgOverlay = L.imageOverlay(imageUrl, imageBounds).addTo(theMap)
+theMap.setMaxBounds(bounds)
+
+let mapTerrs = L.layerGroup().addTo(theMap)
+
+
+terrBounds.forEach(item => {
+    L.polygon(item[1], { color: 'blue' }).addTo(mapTerrs)
+})

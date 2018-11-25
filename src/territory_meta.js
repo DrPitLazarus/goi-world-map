bd550c171ab9fd85da1b035724810fbf08a2ef29
Dr. Pit Lazarus <drpitlazarus@gmail.com>
2018-11-25 14:26:43 -0500

    Add territory meta

---
+++ src/territory_meta.js
@@ -0,0 +1,294 @@
+module.exports = [
+    {
+        capitol: [1483, 235],
+        name: 'Tamblair'
+    },
+    {
+        capitol: [1572, 317],
+        name: 'Kinforth'
+    },
+    {
+        capitol: [1507, 348],
+        name: 'Oaxley'
+    },
+    {
+        capitol: [1513, 549],
+        name: 'Utentana'
+    },
+    {
+        capitol: [1377, 235],
+        name: 'Cusichaca'
+    },
+    {
+        capitol: [1271, 419],
+        name: 'Hongshi Coast'
+    },
+    {
+        capitol: [1238, 588],
+        name: 'Yunin'
+    },
+    {
+        capitol: [1336, 677],
+        name: 'Paritus'
+    },
+    {
+        capitol: [1460, 611],
+        name: 'Hanat'
+    },
+    {
+        capitol: [1548, 749],
+        name: 'Qinqaachi'
+    },
+    {
+        capitol: [1512, 885],
+        name: 'Ulavaar'
+    },
+    {
+        capitol: [1321, 875],
+        name: 'Anvala'
+    },
+    {
+        capitol: [1239, 840],
+        name: 'Virna'
+    },
+    {
+        capitol: [1300, 984],
+        name: 'Urhal'
+    },
+    {
+        capitol: [1445, 1078],
+        name: 'Northlake'
+    },
+    {
+        capitol: [1278, 1148],
+        name: 'Lord\'s Leap'
+    },
+    {
+        capitol: [1250, 1080],
+        name: 'Skyend'
+    },
+    {
+        capitol: [1229, 1150],
+        name: 'Ravenrock'
+    },
+    {
+        capitol: [1430, 1221],
+        name: 'Glowwater'
+    },
+    {
+        capitol: [1162, 1006],
+        name: 'Faberia'
+    },
+    {
+        capitol: [1077, 1190],
+        name: 'Aspara'
+    },
+    {
+        capitol: [1135, 888],
+        name: 'Bannen\'s Rest'
+    },
+    {
+        capitol: [1180, 766],
+        name: 'Fallow'
+    },
+    {
+        capitol: [1077, 799],
+        name: 'Cathedral'
+    },
+    {
+        capitol: [1105, 695],
+        name: 'Aleston'
+    },
+    {
+        capitol: [1141, 604],
+        name: 'Suna'
+    },
+    {
+        capitol: [1147, 394],
+        name: 'Jingshan'
+    },
+    {
+        capitol: [991, 294],
+        name: 'Changning'
+    },
+    {
+        capitol: [1031, 400],
+        name: 'Wu Tower'
+    },
+    {
+        capitol: [1061, 511],
+        name: 'Qinjuru'
+    },
+    {
+        capitol: [1009, 576],
+        name: 'Saltpan'
+    },
+    {
+        capitol: [1026, 685],
+        name: 'Oblivion Pass'
+    },
+    {
+        capitol: [1012, 747],
+        name: 'Garrow'
+    },
+    {
+        capitol: [1019, 871],
+        name: 'Sunder'
+    },
+    {
+        capitol: [1016, 1021],
+        name: 'Lookout'
+    },
+    {
+        capitol: [978, 1152],
+        name: 'Ballast'
+    },
+    {
+        capitol: [886, 1218],
+        name: 'Troydon'
+    },
+    {
+        capitol: [960, 1008],
+        name: 'Boomtown'
+    },
+    {
+        capitol: [924, 936],
+        name: 'Jackson Hole'
+    },
+    {
+        capitol: [769, 908],
+        name: 'Albys'
+    },
+    {
+        capitol: [901, 596],
+        name: 'Orrington'
+    },
+    {
+        capitol: [876, 486],
+        name: 'Sanctuary'
+    },
+    {
+        capitol: [853, 359],
+        name: 'Yao Ling Pass'
+    },
+    {
+        capitol: [894, 235],
+        name: 'Lu Tower'
+    },
+    {
+        capitol: [812, 260],
+        name: 'Bai Hua Hills'
+    },
+    {
+        capitol: [782, 466],
+        name: 'Dragontown'
+    },
+    {
+        capitol: [798, 645],
+        name: 'Faron'
+    },
+    {
+        capitol: [605, 578],
+        name: 'Landmark'
+    },
+    {
+        capitol: [678, 430],
+        name: 'Naufrage'
+    },
+    {
+        capitol: [692, 348],
+        name: 'Sabbia'
+    },
+    {
+        capitol: [704, 211],
+        name: 'Flyaway'
+    },
+    {
+        capitol: [584, 306],
+        name: 'Canon'
+    },
+    {
+        capitol: [505, 382],
+        name: 'Alleron'
+    },
+    {
+        capitol: [482, 578],
+        name: 'Caldera'
+    },
+    {
+        capitol: [462, 743],
+        name: 'Kire'
+    },
+    {
+        capitol: [485, 885],
+        name: 'Sabakumura'
+    },
+    {
+        capitol: [491, 1020],
+        name: 'Selogorod'
+    },
+    {
+        capitol: [562, 1029],
+        name: 'Orlevsela'
+    },
+    {
+        capitol: [631, 1103],
+        name: 'Starostrog'
+    },
+    {
+        capitol: [464, 1142],
+        name: 'Vyshtorg'
+    },
+    {
+        capitol: [680, 1225],
+        name: 'Vama Rea'
+    },
+    {
+        capitol: [552, 1391],
+        name: 'Mor Mare'
+    },
+    {
+        capitol: [681, 1590],
+        name: 'Andelata'
+    },
+    {
+        capitol: [753, 1638],
+        name: 'Lirodunum'
+    },
+    {
+        capitol: [808, 1781],
+        name: 'Beldusios'
+    },
+    {
+        capitol: [970, 1761],
+        name: 'Allonia'
+    },
+    {
+        capitol: [864, 1633],
+        name: 'Lutessa'
+    },
+    {
+        capitol: [1059, 1602],
+        name: 'Averna'
+    },
+    {
+        capitol: [1170, 1579],
+        name: 'Anthos'
+    },
+    {
+        capitol: [1046, 1487],
+        name: 'Lascus'
+    },
+    {
+        capitol: [1151, 1409],
+        name: 'Itonia'
+    },
+    {
+        capitol: [1278, 1326],
+        name: 'Serpent\'s Point'
+    },
+    {
+        capitol: [1228, 1284],
+        name: 'Blackcliff'
+    }
+]

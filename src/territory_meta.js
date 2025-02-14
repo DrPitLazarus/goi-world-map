9ecb0c6a968c56153954889c666f1a7c485254bc
Pit <drpitlazarus@gmail.com>
2025-02-13 21:48:31 -0500

    [territory-meta] Add game territory IDs

--- src/territory_meta.js
+++ src/territory_meta.js
@@ -1,294 +1,367 @@
 export default [
     {
+        id: 1,
         capitol: [1483, 235],
-        name: 'Tamblair'
+        name: 'Tamblair',
     },
     {
+        id: 2,
         capitol: [1572, 317],
-        name: 'Kinforth'
+        name: 'Kinforth',
     },
     {
+        id: 3,
         capitol: [1507, 348],
-        name: 'Oaxley'
+        name: 'Oaxley',
     },
     {
+        id: 4,
         capitol: [1513, 549],
-        name: 'Utentana'
+        name: 'Utentana',
     },
     {
+        id: 5,
         capitol: [1377, 235],
-        name: 'Cusichaca'
+        name: 'Cusichaca',
     },
     {
+        id: 6,
         capitol: [1271, 419],
-        name: 'Hongshi Coast'
+        name: 'Hongshi Coast',
     },
     {
+        id: 55,
         capitol: [1238, 588],
-        name: 'Yunin'
+        name: 'Yunin',
     },
     {
+        id: 56,
         capitol: [1336, 677],
-        name: 'Paritus'
+        name: 'Paritus',
     },
     {
+        id: 8,
         capitol: [1460, 611],
-        name: 'Hanat'
+        name: 'Hanat',
     },
     {
+        id: 7,
         capitol: [1548, 749],
-        name: 'Qinqaachi'
+        name: 'Qinqaachi',
     },
     {
+        id: 9,
         capitol: [1512, 885],
-        name: 'Ulavaar'
+        name: 'Ulavaar',
     },
     {
+        id: 53,
         capitol: [1321, 875],
-        name: 'Anvala'
+        name: 'Anvala',
     },
     {
+        id: 57,
         capitol: [1239, 840],
-        name: 'Virna'
+        name: 'Virna',
     },
     {
+        id: 52,
         capitol: [1300, 984],
-        name: 'Urhal'
+        name: 'Urhal',
     },
     {
+        id: 54,
         capitol: [1445, 1078],
-        name: 'Northlake'
+        name: 'Northlake',
     },
     {
+        id: 47,
         capitol: [1278, 1148],
-        name: 'Lord\'s Leap'
+        name: 'Lord\'s Leap',
     },
     {
+        id: 51,
         capitol: [1250, 1080],
-        name: 'Skyend'
+        name: 'Skyend',
     },
     {
+        id: 50,
         capitol: [1229, 1150],
-        name: 'Ravenrock'
+        name: 'Ravenrock',
     },
     {
+        id: 46,
         capitol: [1430, 1221],
-        name: 'Glowwater'
+        name: 'Glowwater',
     },
     {
+        id: 58,
         capitol: [1162, 1006],
-        name: 'Faberia'
+        name: 'Faberia',
     },
     {
+        id: 59,
         capitol: [1077, 1190],
-        name: 'Aspara'
+        name: 'Aspara',
     },
     {
+        id: 70,
         capitol: [1135, 888],
-        name: 'Bannen\'s Rest'
+        name: 'Bannen\'s Rest',
     },
     {
+        id: 69,
         capitol: [1180, 766],
-        name: 'Fallow'
+        name: 'Fallow',
     },
     {
+        id: 73,
         capitol: [1077, 799],
-        name: 'Cathedral'
+        name: 'Cathedral',
     },
     {
+        id: 68,
         capitol: [1105, 695],
-        name: 'Aleston'
+        name: 'Aleston',
     },
     {
+        id: 67,
         capitol: [1141, 604],
-        name: 'Suna'
+        name: 'Suna',
     },
     {
+        id: 10,
         capitol: [1147, 394],
-        name: 'Jingshan'
+        name: 'Jingshan',
     },
     {
+        id: 13,
         capitol: [991, 294],
-        name: 'Changning'
+        name: 'Changning',
     },
     {
+        id: 12,
         capitol: [1031, 400],
-        name: 'Wu Tower'
+        name: 'Wu Tower',
     },
     {
+        id: 11,
         capitol: [1061, 511],
-        name: 'Qinjuru'
+        name: 'Qinjuru',
     },
     {
+        id: 66,
         capitol: [1009, 576],
-        name: 'Saltpan'
+        name: 'Saltpan',
     },
     {
+        id: 65,
         capitol: [1026, 685],
-        name: 'Oblivion Pass'
+        name: 'Oblivion Pass',
     },
     {
+        id: 72,
         capitol: [1012, 747],
-        name: 'Garrow'
+        name: 'Garrow',
     },
     {
+        id: 71,
         capitol: [1019, 871],
-        name: 'Sunder'
+        name: 'Sunder',
     },
     {
+        id: 60,
         capitol: [1016, 1021],
-        name: 'Lookout'
+        name: 'Lookout',
     },
     {
+        id: 37,
         capitol: [978, 1152],
-        name: 'Ballast'
+        name: 'Ballast',
     },
     {
+        id: 28,
         capitol: [886, 1218],
-        name: 'Troydon'
+        name: 'Troydon',
     },
     {
+        id: 61,
         capitol: [960, 1008],
-        name: 'Boomtown'
+        name: 'Boomtown',
     },
     {
+        id: 62,
         capitol: [924, 936],
-        name: 'Jackson Hole'
+        name: 'Jackson Hole',
     },
     {
+        id: 63,
         capitol: [769, 908],
-        name: 'Albys'
+        name: 'Albys',
     },
     {
+        id: 64,
         capitol: [901, 596],
-        name: 'Orrington'
+        name: 'Orrington',
     },
     {
+        id: 14,
         capitol: [876, 486],
-        name: 'Sanctuary'
+        name: 'Sanctuary',
     },
     {
+        id: 16,
         capitol: [853, 359],
-        name: 'Yao Ling Pass'
+        name: 'Yao Ling Pass',
     },
     {
+        id: 15,
         capitol: [894, 235],
-        name: 'Lu Tower'
+        name: 'Lu Tower',
     },
     {
+        id: 17,
         capitol: [812, 260],
-        name: 'Bai Hua Hills'
+        name: 'Bai Hua Hills',
     },
     {
+        id: 18,
         capitol: [782, 466],
-        name: 'Dragontown'
+        name: 'Dragontown',
     },
     {
+        id: 22,
         capitol: [798, 645],
-        name: 'Faron'
+        name: 'Faron',
     },
     {
+        id: 25,
         capitol: [605, 578],
-        name: 'Landmark'
+        name: 'Landmark',
     },
     {
+        id: 21,
         capitol: [678, 430],
-        name: 'Naufrage'
+        name: 'Naufrage',
     },
     {
+        id: 20,
         capitol: [692, 348],
-        name: 'Sabbia'
+        name: 'Sabbia',
     },
     {
+        id: 19,
         capitol: [704, 211],
-        name: 'Flyaway'
+        name: 'Flyaway',
     },
     {
+        id: 23,
         capitol: [584, 306],
-        name: 'Canon'
+        name: 'Canon',
     },
     {
+        id: 24,
         capitol: [505, 382],
-        name: 'Alleron'
+        name: 'Alleron',
     },
     {
+        id: 26,
         capitol: [482, 578],
-        name: 'Caldera'
+        name: 'Caldera',
     },
     {
+        id: 27,
         capitol: [462, 743],
-        name: 'Kire'
+        name: 'Kire',
     },
     {
+        id: 29,
         capitol: [485, 885],
-        name: 'Sabakumura'
+        name: 'Sabakumura',
     },
     {
+        id: 31,
         capitol: [491, 1020],
-        name: 'Selogorod'
+        name: 'Selogorod',
     },
     {
+        id: 30,
         capitol: [562, 1029],
-        name: 'Orlevsela'
+        name: 'Orlevsela',
     },
     {
+        id: 32,
         capitol: [631, 1103],
-        name: 'Starostrog'
+        name: 'Starostrog',
     },
     {
+        id: 33,
         capitol: [464, 1142],
-        name: 'Vyshtorg'
+        name: 'Vyshtorg',
     },
     {
+        id: 34,
         capitol: [680, 1225],
-        name: 'Vama Rea'
+        name: 'Vama Rea',
     },
     {
+        id: 35,
         capitol: [552, 1391],
-        name: 'Mor Mare'
+        name: 'Mor Mare',
     },
     {
+        id: 36,
         capitol: [681, 1590],
-        name: 'Andelata'
+        name: 'Andelata',
     },
     {
+        id: 45,
         capitol: [753, 1638],
-        name: 'Lirodunum'
+        name: 'Lirodunum',
     },
     {
+        id: 44,
         capitol: [808, 1781],
-        name: 'Beldusios'
+        name: 'Beldusios',
     },
     {
+        id: 43,
         capitol: [970, 1761],
-        name: 'Allonia'
+        name: 'Allonia',
     },
     {
+        id: 42,
         capitol: [864, 1633],
-        name: 'Lutessa'
+        name: 'Lutessa',
     },
     {
+        id: 41,
         capitol: [1059, 1602],
-        name: 'Averna'
+        name: 'Averna',
     },
     {
+        id: 40,
         capitol: [1170, 1579],
-        name: 'Anthos'
+        name: 'Anthos',
     },
     {
+        id: 39,
         capitol: [1046, 1487],
-        name: 'Lascus'
+        name: 'Lascus',
     },
     {
+        id: 38,
         capitol: [1151, 1409],
-        name: 'Itonia'
+        name: 'Itonia',
     },
     {
+        id: 49,
         capitol: [1278, 1326],
-        name: 'Serpent\'s Point'
+        name: 'Serpent\'s Point',
     },
     {
+        id: 48,
         capitol: [1228, 1284],
-        name: 'Blackcliff'
+        name: 'Blackcliff',
     }
 ]

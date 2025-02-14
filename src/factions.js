810934a27ed0d80d892b4ba11681fc481f667ba3
Pit <drpitlazarus@gmail.com>
2025-02-13 23:56:24 -0500

    [factions] Add game faction IDs.

--- src/factions.js
+++ src/factions.js
@@ -1,25 +1,31 @@
 export default {
   anglea: {
+    id: 2,
     name: "Anglean Republic",
     color: "#034f7d",
   },
   baron: {
+    id: 1,
     name: "Fjord Baronies",
     color: "#56317d",
   },
   chaladon: {
+    id: 3,
     name: "Order of Chaladon",
     color: "#487242",
   },
   merchant: {
+    id: 4,
     name: "Mercantile Guild",
     color: "#8f8927",
   },
   arashi: {
+    id: 5,
     name: "Arashi League",
     color: "#674700",
   },
   yesha: {
+    id: 6,
     name: "Yesha Empire",
     color: "#66160d",
   },

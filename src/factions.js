a6c83961b4fd58427c2192c03d4fcbad0ff90f6e
Pit <drpitlazarus@gmail.com>
2025-02-20 00:26:03 -0500

    [factions] Change from object to array and add Independent faction - And sort in order.

--- src/factions.js
+++ src/factions.js
@@ -1,32 +1,37 @@
-export default {
-  anglea: {
-    id: 2,
-    name: "Anglean Republic",
-    color: "#034f7d",
+export default [
+  {
+    id: 0,
+    name: "Independent",
+    color: "#AAA",
   },
-  baron: {
+  {
     id: 1,
     name: "Fjord Baronies",
     color: "#56317d",
   },
-  chaladon: {
+  {
+    id: 2,
+    name: "Anglean Republic",
+    color: "#034f7d",
+  },
+  {
     id: 3,
     name: "Order of Chaladon",
     color: "#487242",
   },
-  merchant: {
+  {
     id: 4,
     name: "Mercantile Guild",
     color: "#8f8927",
   },
-  arashi: {
+  {
     id: 5,
     name: "Arashi League",
     color: "#674700",
   },
-  yesha: {
+  {
     id: 6,
     name: "Yesha Empire",
     color: "#66160d",
   },
-};
+];
